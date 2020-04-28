1beb2a3b32c32550401bcf097c79167217e3ee7b
--help <domsbuhendwa2@gmail.com>
2020-04-28 07:02:50 +0200

    setup user logout action in reducer

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom"
 import { Button } from "semantic-ui-react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
-
-const HomePage = ({ isAuthenticated }) => (
+import * as actions from "../../actions/auth"
+const HomePage = ({ isAuthenticated, logout }) => (
   <div className="ui container">
     <h1>Home Page</h1>
     {isAuthenticated ? (
-      <Button primary>Logout</Button>
+      <Button primary onClick={() => logout()}>
+        Logout
+      </Button>
     ) : (
       <Link to="/login">Login</Link>
     )}
@@ -17,12 +19,14 @@ const HomePage = ({ isAuthenticated }) => (
 
 HomePage.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired,
 }
 
 function mapStateToProps(state) {
   return {
     isAuthenticated: !!state.user.token,
+    logout: PropTypes.func.isRequired,
   }
 }
 
-export default connect(mapStateToProps)(HomePage)
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage)
